21e92f9a2cea1fa0f1275cf6f9cc79ef1230442d
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-06-16 18:46:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,32 +6,3 @@ terraform {
     }
   }
 }
-
-terraform {
-  backend "remote" {
-    hostname = "tfe.eastus.cloudapp.azure.com"
-    organization = "DanielTest"
-
-    workspaces {
-      name = "june-06-WS-03"
-    }
-  }
-}
-
-provider "azurerm" {
-  features{}
-  
-  subscription_id = ""
-  tenant_id = ""
-  client_id = ""
-  client_secret = ""
-}
-
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "June-TFC-Org-THREE"
-  location = "canadaeast"
-
-  tags = {
-    environment = "Production"
-  }
-}
